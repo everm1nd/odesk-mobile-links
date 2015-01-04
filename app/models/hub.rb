2da3204d611415c82cb92f968145f4ce7087f9f6
everm1nd <everm1nd.mail@gmail.com>
2015-01-04 23:22:38 +0700

    Revert "added Link model" and "a little metaprogramming in Hub#links method" Oh, fuck. That's was a bad naming decision.
    This reverts commit 5ec5d09facc35b59b2c4f976cdd36ba84bdd69d9.

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,11 +1,3 @@
 class Hub < ActiveRecord::Base
-  has_many :links
-
-  def links=(*args)
-    if args.count == 1 and args.first.is_a?(String)
-      # TODO: parse string here
-    else
-      super(*args)
-    end
-  end
+	attr_accessor :links
 end
